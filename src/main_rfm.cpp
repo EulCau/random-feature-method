73e76d08afb56be8c5bf35f78eab405bcfc20668
EulCau <lx_0318@mail.ustc.edu.cn>
2025-10-19 17:21:50 +0800

    Create main rfm

--- src/main_rfm.cpp
+++ src/main_rfm.cpp
@@ -1,3 +1,18 @@
-//
-// Created by eul_cau on 2025/8/27.
-//
+# include "rfm_solver.h"
+#include "config.h"
+#include "equation_factory.h"
+#include "register_all_eqn.h"
+#include <iostream>
+
+int main()
+{
+    force_link_all_equations();
+    const Config cfg = load_config("hjb_lq_d100.json");
+
+    const auto pde = EquationFactory::instance().create(cfg.eqn_config.eqn_name, cfg.eqn_config);
+    SolveResult solve(cfg, pde);
+    std::cout << solve.alpha << std::endl;
+    std::cout << solve.terminal_err << std::endl;
+
+    return 0;
+}
