dcda4209dbf40cdd58076df6a2aa6e422d9717c8
EulCau <lx_0318@mail.ustc.edu.cn>
2026-01-29 17:43:51 +0800

    Rewrite main for new skeleton (just placeholder for now)

--- src/main_rfm.cpp
+++ src/main_rfm.cpp
@@ -6,13 +6,15 @@
 
 int main()
 {
+    uint64_t seed = 42;
     force_link_all_equations();
     const Config cfg = load_config("***"); // TODO: linear equation
-
     const auto pde = EquationFactory::instance().create(cfg.eqn_config.eqn_name, cfg.eqn_config);
-    auto result = solve(cfg, pde);
-    std::cout << result.alpha << std::endl;
-    std::cout << result.terminal_err << std::endl;
+
+    auto rfm_solver = RFMSolver(cfg, pde, seed);
+
+    float result = 0.0f;
+    std::cout << result << std::endl;
 
     return 0;
 }
